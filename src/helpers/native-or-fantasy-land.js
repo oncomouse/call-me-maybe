bd85226223bacbf3422d7b6637f06b62b6ed2ad2
oncomouse <oncomouse@gmail.com>
2018-01-12 16:38:25 -0600

    Add a test for null

--- src/helpers/native-or-fantasy-land.js
+++ src/helpers/native-or-fantasy-land.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const nativeOrFantasyLand = (target, method) => {
-    if (typeof target[`fantasy-land/${method}`] === 'function') {
+    if(target == null) {
+        // Do nothing for now (throw error below)
+    } else if (typeof target[`fantasy-land/${method}`] === 'function') {
         return `fantasy-land/${method}`
     } else if (typeof target[method] === 'function') {
         return method
-    } else {
-        throw new TypeError(`Could not find ${target}#${method} or ${target}#fantasy-land/${method}.`)
     }
+    throw new TypeError(`Could not find ${target}#${method} or ${target}#fantasy-land/${method}.`)
 }
 export default nativeOrFantasyLand
